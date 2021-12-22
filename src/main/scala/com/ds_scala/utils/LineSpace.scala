439901104e06b16be41ba03e39d2098992a84a60
Alex <a.giavaras@gmail.com>
2021-12-22 18:05:30 +0000

    Remove not used functionality and improve implementation

--- src/main/scala/com/ds_scala/utils/LineSpace.scala
+++ src/main/scala/com/ds_scala/utils/LineSpace.scala
@@ -4,44 +4,12 @@ import breeze.linalg.DenseVector
 
 object LineSpace {
 
-  /**
-   * Partition the interval [a, b] into n parts
-   * @param a start of interval
-   * @param b end of interval
-   * @param n Number of parts
-   * @return The discretized points
-   */
-  def partition(a: Double, b: Double, n: Int): DenseVector[Double] = {
-
-    require(a < b)
-    require( n > 2)
-    val points: DenseVector[Double] = DenseVector.zeros[Double](n)
-
-    val dx = (b - a)/n
-    points(0) = a
-    points(n-1) = b
-    for(p <- 1 to n - 2){
-      points(p) = a + p*dx
-    }
-
-    points
-  }
-
   def partitionWithWhiteNoise(a: Double, b: Double, n: Int) : DenseVector[Double] = {
 
     require(a < b)
     require( n > 2)
-    val points: DenseVector[Double] = DenseVector.zeros[Double](n)
+    val lineSpace = breeze.linalg.linspace(a, b, length = n)
     val g = breeze.stats.distributions.Gaussian(0,1)
-
-    val dx = (b - a)/n
-    points(0) = a
-    points(n-1) = b
-    for(p <- 1 to n - 2){
-      points(p) = a + p*dx + g.sample()
-    }
-
-    points
-
+    lineSpace + DenseVector[Double](g.sample(n))
   }
 }
